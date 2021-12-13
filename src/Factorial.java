2ee8f63bfebff00359999f04f52716400113ced9
Jonma <jmartinla21dw@ikzubirimanteo.com>
2021-12-13 13:01:51 +0100

    Metido todo el texto

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,5 +1,16 @@
 
 public class Factorial {
-
-	
+	public static void main( String[] args ) {
+	      int numero = 9;
+	      System.out.println( "El factorial de " + numero + " es: " + factorial( numero ) );      
+	   }
+	 
+	   public static int factorial( int numero ) {
+	      int fact = 1;
+	      for( int i = 1; i <= numero; i++ ) {
+	         fact *= i;
+	      }
+	 
+	      return fact;
+	   }
 }

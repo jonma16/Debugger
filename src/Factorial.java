4d0ca2a12fb9037b204063426d2a88acdbfb1002
Jonma <jmartinla21dw@ikzubirimanteo.com>
2021-12-13 13:25:02 +0100

    Creado fallo

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,7 +2,7 @@
 public class Factorial {
 	public static void main( String[] args ) {
 	      int numero = 9;
-	      System.out.println( "El factorial de " + numero + " es: " + factorial( numero ) );      
+	      System.out.println( "El factorial de " + numero + " es: " + factoria( numero ) );      
 	   }
 	 
 	   public static int factorial( int numero ) {
